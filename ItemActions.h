988a1168d846a20b107c0f757641361250e167b5
Jesse Snyder <jesse.d.snyder@gmail.com>
2018-04-17 03:49:16 +0000

    1. Fixed issue Chatwiththisname brought up that would cause it to spam loot actions over and over again. 2. Added the ability to do looting during raid.  /AutoLoot raidloot [on|off] -> Toggle raid looting on and off, the default is off. 3. Rewrote item actions such as /autoloot barter/sell/deposit to make the code cleaner (at least in my own opinion).

---
+++ ItemActions.h
@@ -0,0 +1,39 @@
+#define PERSONALBANKER_CLASS		40
+#define MERCHANT_CLASS				41
+#define GUILDBANKER_CLASS			66
+
+#include <chrono>
+
+using namespace std;
+typedef std::chrono::high_resolution_clock pluginclock;
+
+// Variables necessary to deposit/sell/barter/etc items
+extern bool							bBarterActive;
+extern bool							bDepositActive;
+extern bool							bSellActive;
+extern bool							bBuyActive;
+extern bool							bBarterReset; // Set to true when you need to refresh your barter search
+extern bool							bBarterItemSold; // Set to true when you sell an item
+extern bool							bEndThreads; // Set to true when you want to end any threads out there, also it is used to enforce that at most a single thread is active at one time
+extern pluginclock::time_point		LootThreadTimer;
+
+// Functions necessary to deposit/sell/barter/etc items
+bool MoveToNPC(PSPAWNINFO pSpawn);
+bool HandleMoveUtils(void);  // Used to connect to MQ2MoveUtils
+bool OpenWindow(PSPAWNINFO pSpawn);
+void SellItem(PITEMINFO theitem);
+bool FitInPersonalBank(PITEMINFO pItem);
+void PutInPersonalBank(PITEMINFO pItem);
+bool CheckGuildBank(PITEMINFO pItem);
+bool PutInGuildBank(PITEMINFO pItem);
+bool DepositItems(PITEMINFO pItem);
+void SetItemPermissions(PITEMINFO pItem);
+void BarterSearch(int nBarterItems, CHAR* pzItemName, DWORD	MyBarterMinimum, CListWnd *cListInvWnd);
+int FindBarterIndex(DWORD MyBarterMinimum, CListWnd *cBuyLineListWnd);
+bool SelectBarterSell (int BarterMaximumIndex, CListWnd *cBuyLineListWnd);
+void SelectBarterQuantity(int BarterMaximumIndex, CHAR* pszItemName, CListWnd *cBuyLineListWnd);
+DWORD __stdcall SellItems(PVOID pData);
+DWORD __stdcall BuyItem(PVOID pData);
+DWORD __stdcall DepositPersonalBanker(PVOID pData);
+DWORD __stdcall DepositGuildBanker(PVOID pData);
+DWORD __stdcall BarterItems(PVOID pData);
