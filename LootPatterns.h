27aa688f7745208dea84c153039d52d5be661b3e
Jesse Snyder <jesse.d.snyder@gmail.com>
2019-01-15 23:02:03 +0000

    Updated to version 1.12 1. When items are marked “destroy” MQ2Autoloot will ignore your “SaveBagSlots” limit and will keep looting and destroying items.  This means that if you want to clear all the corpses in your camp your loot.ini needs to have SaveBagSlots greater then 0. 2. There is a new loot setting. “NewItemDelay”, this allows people who are playing manually to pass out any new items before the plugin decides where they go.  The command to change this setting is: /AutoLoot newitemdelay #n -> Master looter waits #n seconds when a new item drops before looting that item. 3. Fixed CTD related to “/autoloot buy “Item Name” #Quantity” command. 4. Removed the patterns code stuff by eqholic, it was only implemented for people not grouped. Since no one was using it, I pulled the code out.

--- LootPatterns.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void read_loot_patterns(const char* inifile, void(*report)(const char*));
-void forget_loot_patterns();
-void list_loot_patterns(void(report)(const char *));
-bool action_from_loot_patterns(const char* lootname, char* action, int max_size);
