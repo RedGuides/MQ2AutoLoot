52ad0ffb1617a4ec972a26e62716492655b84fe1
Jesse Snyder <jesse.d.snyder@gmail.com>
2019-02-13 22:16:44 +0000

    1. MQ2Autoloot will now use greed instead of need. 2. Non-ML characters will now check how many items have been selected need/greed and will pause checking boxes when that is equal to their free inventory number.  Before they went through the whole list as long as they had one open slot.  This should fix the issue with non-ML characters getting items stuck on their cursors. 3. Fixed a bug with passing out loot that would cause the wrong item to be distributed. 4. Fixed a bug that caused items to be passed out when they shouldn't have. 5. Fixed an issue when your ML has a lore item in parcels and the item dropped and your ML wanted it, this was due to the lore check not being able to determine what is stored in parcels.  Tone, stop using parcels for storage. 6. Fixed some issues with NewItemDelay.  The ML is now the only one that can create new loot entries while the advanced looting window is open if NewItemDelay > 0.  This fixes the issue where a non-ML toon creates the entry before the ML does, thus not giving you time to determine what to do with the item.  Non-ML characters will check after all the items are cleared to make sure that their is an entry for new items, this will solve any issues related to using multiple loot ini files.

--- MQ2AutoLoot.h
+++ MQ2AutoLoot.h
@@ -1,9 +1,11 @@
 #define	PLUGIN_CHAT_MSG		"\ag[MQ2AutoLoot]\ax "  
 #define	PLUGIN_DEBUG_MSG	"[MQ2AutoLoot] "  
 
-#include <chrono>
-
 using namespace std;
+#include <map>
+#include <chrono>
+#include <string>
+
 typedef std::chrono::high_resolution_clock pluginclock;
 
 // Variables that are setable through the /AutoLoot command
@@ -36,20 +38,24 @@ void ShowInfo(void); // Outputs the variable settings into your MQ2 Window
 bool CheckCursor(void);  // Returns true if an item is on your cursor
 bool DestroyStuff(void); // Will find items you loot marked destroy and delete them
 bool CheckWindows(bool ItemOnCursor);  // Returns true if your attempting to accept trade requests or click the confirmation box for no drop items
+void ClearLootEntries(void); // Clears out the LootEntries from the non-ML toons
 bool SetLootSettings(void); // Turn off Auto Loot All
 bool HandlePersonalLoot(bool ItemOnCursor, PCHARINFO pChar, PCHARINFO2 pChar2, PEQADVLOOTWND pAdvLoot, CListWnd *pPersonalList, CListWnd *pSharedList); // Handle items in your personal loot window
 bool HandleSharedLoot(bool ItemOnCursor, PCHARINFO pChar, PCHARINFO2 pChar2, PEQADVLOOTWND pAdvLoot, CListWnd *pPersonalList, CListWnd *pSharedList); // Handle items in your shared loot window
 bool WinState(CXWnd *Wnd);
 PMQPLUGIN Plugin(char* PluginName);
 bool HandleEQBC(void);  // Used to get EQBC Names
-bool ParseLootEntry(bool ItemOnCursor, PCHARINFO2 pChar2, PLOOTITEM pLootItem, bool bMasterLooter, char* pszItemAction, bool* pbIWant, bool* pbCheckIfOthersWant); // Parse loot entry for an item and determine an appropriate action
-void InitialLootEntry(PLOOTITEM pLootItem); // Create the initial loot entry for a new item or one where we don't recognize the loot action as valid
+bool AmITheMasterLooter(PCHARINFO pChar, bool* pbMasterLooter); // Determines if the character is the master looter, if it has to set a master looter it instructs the plugin to pause looting
+bool ParseLootEntry(bool ItemOnCursor, PCHARINFO pChar, PCHARINFO2 pChar2, PLOOTITEM pLootItem, bool bMasterLooter, char* pszItemAction, bool* pbIWant, bool* pbCheckIfOthersWant); // Parse loot entry for an item and determine an appropriate action
+void InitialLootEntry(PLOOTITEM pLootItem, bool bCreateImmediately); // Create the initial loot entry for a new item or one where we don't recognize the loot action as valid
 bool DoIHaveSpace(CHAR* pszItemName, DWORD plMaxStackSize, DWORD pdStackSize, bool bSaveBagSlots);
 bool FitInInventory(DWORD pdItemSize);
 int CheckIfItemIsLoreByID(int ItemID);
 DWORD FindItemCount(CHAR* pszItemName);
 DWORD __stdcall PassOutLoot(PVOID pData); // Pass out items to the group or the raid
-bool DistributeLoot(CHAR* szName, PLOOTITEM pShareItem);
+bool DistributeLoot(CHAR* szName, PLOOTITEM pShareItem, LONG ItemIndex);
+bool ContinueCheckingSharedLoot(void); // Will stop looping through the shared loot list when the number of items clicked need/greed is equal or exceeds the number of free inventory slots
+int AutoLootFreeInventory(void); // used to calculate free inventory
 bool DirectoryExists(LPCTSTR lpszPath);
 void CreateLogEntry(PCHAR szLogEntry);
 void AutoLootCommand(PSPAWNINFO pCHAR, PCHAR szLine);
@@ -57,4 +63,4 @@ LONG SetBOOL(long Cur, PCHAR Val, PCHAR Sec = "", PCHAR Key = "", PCHAR INI = ""
 void SetItemCommand(PSPAWNINFO pCHAR, PCHAR szLine);
 void CreateLootEntry(CHAR* szAction, CHAR* szEntry, PITEMINFO pItem);
 int dataAutoLoot(char* szName, MQ2TYPEVAR &Ret);
-int AutoLootFreeInventory(void); // used to calculate TLO ${AutoLoot.FreeInventory}
+
