12a54120cab468fd71526da92ffffe9b080c6ff7
Knightly <knightly.cavalier@gmail.com>
2020-07-04 23:17:11 -1000

    Update for Next

--- MQ2AutoLoot.h
+++ MQ2AutoLoot.h
@@ -45,20 +45,19 @@ bool DestroyStuff(); // Will find items you loot marked destroy and delete them
 bool CheckWindows(bool ItemOnCursor);  // Returns true if your attempting to accept trade requests or click the confirmation box for no drop items
 void ClearLootEntries(); // Clears out the LootEntries from the non-ML toons
 bool SetLootSettings(); // Turn off Auto Loot All
-bool HandlePersonalLoot(bool ItemOnCursor, PCHARINFO pChar, PCHARINFO2 pChar2, PEQADVLOOTWND pAdvLoot, CListWnd *pPersonalList, CListWnd *pSharedList); // Handle items in your personal loot window
-bool HandleSharedLoot(bool ItemOnCursor, PCHARINFO pChar, PCHARINFO2 pChar2, PEQADVLOOTWND pAdvLoot, CListWnd *pPersonalList, CListWnd *pSharedList); // Handle items in your shared loot window
+bool HandlePersonalLoot(bool ItemOnCursor, PCHARINFO pChar, PcProfile* pChar2, CAdvancedLootWnd* pAdvLoot, CListWnd *pPersonalList, CListWnd *pSharedList); // Handle items in your personal loot window
+bool HandleSharedLoot(bool ItemOnCursor, PCHARINFO pChar, PcProfile* pChar2, CAdvancedLootWnd* pAdvLoot, CListWnd *pPersonalList, CListWnd *pSharedList); // Handle items in your shared loot window
 bool WinState(CXWnd *Wnd);
-PMQPLUGIN Plugin(char* PluginName);
 bool HandleEQBC();  // Used to get EQBC Names
 bool AmITheMasterLooter(PCHARINFO pChar, bool* pbMasterLooter); // Determines if the character is the master looter, if it has to set a master looter it instructs the plugin to pause looting
-bool ParseLootEntry(bool ItemOnCursor, PCHARINFO pChar, PCHARINFO2 pChar2, PLOOTITEM pLootItem, bool bMasterLooter, char* pszItemAction, bool* pbIWant, bool* pbCheckIfOthersWant); // Parse loot entry for an item and determine an appropriate action
-void InitialLootEntry(PLOOTITEM pLootItem, bool bCreateImmediately); // Create the initial loot entry for a new item or one where we don't recognize the loot action as valid
+bool ParseLootEntry(bool ItemOnCursor, PCHARINFO pChar, PcProfile* pChar2, AdvancedLootItem* pLootItem, bool bMasterLooter, char* pszItemAction, bool* pbIWant, bool* pbCheckIfOthersWant); // Parse loot entry for an item and determine an appropriate action
+void InitialLootEntry(AdvancedLootItem* pLootItem, bool bCreateImmediately); // Create the initial loot entry for a new item or one where we don't recognize the loot action as valid
 bool DoIHaveSpace(CHAR* pszItemName, DWORD plMaxStackSize, DWORD pdStackSize, bool bSaveBagSlots);
 bool FitInInventory(DWORD pdItemSize);
 int  CheckIfItemIsLoreByID(int64_t ItemID);
 int FindItemCount(CHAR* pszItemName);
 DWORD __stdcall PassOutLoot(PVOID pData); // Pass out items to the group or the raid
-bool DistributeLoot(CHAR* szName, PLOOTITEM pShareItem, LONG ItemIndex);
+bool DistributeLoot(CHAR* szName, AdvancedLootItem* pShareItem, LONG ItemIndex);
 bool ContinueCheckingSharedLoot(); // Will stop looping through the shared loot list when the number of items clicked need/greed is equal or exceeds the number of free inventory slots
 int AutoLootFreeInventory(); // used to calculate free inventory
 bool DirectoryExists(LPCTSTR lpszPath);
@@ -67,4 +66,4 @@ void AutoLootCommand(PSPAWNINFO pCHAR, PCHAR szLine);
 LONG SetBOOL(long Cur, PCHAR Val, PCHAR Sec = "", PCHAR Key = "", PCHAR INI = "");
 void SetItemCommand(PSPAWNINFO pCHAR, PCHAR szLine);
 void CreateLootEntry(CHAR* szAction, CHAR* szEntry, PITEMINFO pItem);
-int dataAutoLoot(char* szName, MQ2TYPEVAR &Ret);
+bool dataAutoLoot(const char* szName, MQTypeVar &Ret);
